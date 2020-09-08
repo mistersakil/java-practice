d8774056aa0706151b2ff2f237b51453ab02189a
Saki Mahmud <sakilorb@icloud.com>
2020-09-08 18:46:41 +0600

    Updated hello class

--- Hello.java
+++ Hello.java
@@ -1,9 +1,14 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello java how are you today? ");
-		int x = Integer.parseInt(args[0]);
-		int y = Integer.parseInt(args[1]);
-		System.out.println("The sum of two numbers = "+ (x+y));
+		// int x = Integer.parseInt(args[0]);
+		// int y = Integer.parseInt(args[1]);
+
+		int sum = 0;
+		for(int i = 0; i < args.length; i++){
+			sum += Integer.parseInt(args[i]);
+		}
+		System.out.println("The sum of two numbers = "+ sum);
 
 	}
 }
