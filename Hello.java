cd147a3905e8ec582ddeca9f39ad74d06e76917d
Saki Mahmud <sakilorb@icloud.com>
2020-09-08 16:35:35 +0600

    created sub on main functions arguments

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello java");
+		System.out.println("Hello java how are you today? ");
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		System.out.println("The sum of two numbers = "+ (x+y));
+
 	}
 }
